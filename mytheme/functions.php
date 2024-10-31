ab47a62c38ac1ac460d1c11ffc8c4a4e655921a5
ghadi <ghadi.elias@innowise.com>
2024-10-31 17:45:57 +0300

    task9

--- mytheme/functions.php
+++ mytheme/functions.php
@@ -34,56 +34,6 @@ function enqueue_custom_scripts() {
         'ajax_url' => admin_url('admin-ajax.php')
     ));
 }
-
-
-<<<<<<< Updated upstream
-//function create_product_post_type() {
-//    $labels = array(
-//        'name' => __( 'Products' ),
-//        'singular_name' => __( 'Product' ),
-//        'menu_name' => __( 'Products' ),
-//        'name_admin_bar' => __( 'Product' ),
-//        'add_new' => __( 'Add New Product' ),
-//        'add_new_item' => __( 'Add New Product' ),
-//        'new_item' => __( 'New Product' ),
-//        'edit_item' => __( 'Edit Product' ),
-//        'view_item' => __( 'View Product' ),
-//        'all_items' => __( 'All Products' ),
-//        'search_items' => __( 'Search Products' ),
-//        'parent_item_colon' => __( 'Parent Products:' ),
-//        'not_found' => __( 'No products found.' ),
-//        'not_found_in_trash' => __( 'No products found in Trash.' )
-//    );
-//
-//    $args = array(
-//        'labels' => $labels,
-//        'public' => true,
-//        'has_archive' => true,
-//        'supports' => array( 'thumbnail' ), // Exclude default fields like title and editor
-//        'menu_position' => 5,
-//        'menu_icon' => 'dashicons-cart',
-//        'rewrite' => array( 'slug' => 'products' ),
-//    );
-//
-//    register_post_type( 'product', $args );
-//}
-//
-//add_action( 'init', 'create_product_post_type' );
-=======
-function enable_comments_for_products() {
-    add_post_type_support('storeproduct', 'comments');
-}
-add_action('init', 'enable_comments_for_products');
-
-
-function start_session() {
-    if (!session_id()) {
-        session_start();
-    }
-}
-add_action('init', 'start_session');
-
-// Handle adding items to the cart
 function handle_add_to_cart() {
     if (isset($_POST['product'])) {
         $product = $_POST['product'];
@@ -91,16 +41,14 @@ function handle_add_to_cart() {
 
         $productExists = false;
 
-        foreach ($cart as &$item) { // Use reference to modify the original array
+        foreach ($cart as &$item) {
             if ($item['id'] === $product['id']) {
-                // Product found, increase quantity
                 $item['quantity'] += 1;
                 $productExists = true;
                 break;
             }
         }
 
-        // If product does not exist, add it to the cart
         if (!$productExists) {
             $product['quantity'] = 1;
             $cart[] = $product;
@@ -119,20 +67,17 @@ function handle_add_to_cart() {
 add_action('wp_ajax_add_to_cart', 'handle_add_to_cart');
 add_action('wp_ajax_nopriv_add_to_cart', 'handle_add_to_cart');
 
-// Handle updating items in the cart
 function handle_update_cart() {
     if (isset($_POST['productId']) && isset($_POST['quantity'])) {
         $productId = $_POST['productId'];
         $quantity = intval($_POST['quantity']);
         $cart = isset($_SESSION['cart']) ? $_SESSION['cart'] : [];
 
-        // If quantity is zero or less, remove the item from the cart
         if ($quantity <= 0) {
             $cart = array_filter($cart, function($item) use ($productId) {
                 return $item['id'] !== $productId;
             });
         } else {
-            // Update quantity if the product exists
             foreach ($cart as &$item) {
                 if ($item['id'] === $productId) {
                     $item['quantity'] = $quantity;
@@ -174,6 +119,4 @@ function handle_remove_from_cart() {
 }
 add_action('wp_ajax_remove_from_cart', 'handle_remove_from_cart');
 add_action('wp_ajax_nopriv_remove_from_cart', 'handle_remove_from_cart');
-
->>>>>>> Stashed changes
 
